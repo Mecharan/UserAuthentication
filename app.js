439a03f3ad4db192b7299d24d828f069eb1591d7
Mecharan <saicharanreddym.7@gmail.com>
2022-03-09 20:07:24 +0530

    5th level cookies and sessions with passport passport-local passport-local-mongoose and express-session

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { userInfo } = require("os");
 const req = require("express/lib/request");
-const bcrypt = require("bcrypt");
-const saltRounds = 12;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { serializeUser } = require('passport/lib');
 
 
 const app = express();
@@ -15,6 +17,15 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+app.use(session({
+    secret: 'Super ra happy ga undu',
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
@@ -22,9 +33,24 @@ const userSchema = new mongoose.Schema({
     password : String
 })
 
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
 
 
 app.listen(3000, function(){
@@ -43,37 +69,56 @@ app.get("/register",function(req,res){
     res.render("register");
 })
 
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+})
+
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated())
+        res.render("secrets");
+    else
+        res.redirect("/login");
+})
+
 
 app.post("/register", function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email : req.body.username,
-            password : hash
-        })
-        newUser.save(function(err){
-            if(err)
-                console.log(err);
-            else
-                res.render("secrets");
-        })
-    });
+    User.register({username : req.body.username}, req.body.password, function(err,user){
+        if(err)
+        {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else
+        {
+            passport.authenticate("local")(req,res,function (){
+                res.redirect("/secrets");
+            });
+        }
+    })
     
 })
 
 app.post("/login", function(req,res){
-    User.findOne({email : req.body.username}, function(err,foundUser){
+    const user = new User({
+        email : req.body.username,
+        password : req.body.password
+    })
+
+    req.login(user, function(err){
         if(err)
-            console.log(err);
-        else if(foundUser)
         {
-            bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-                if(result)
-                    res.render("secrets");
-            });
-            
+            console.log(err);
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
         }
     })
 })
+
+
 
 // try nvm for node version management
